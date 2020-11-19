0d4f767e5a4070b03bdd2ace035226091c8d9136
Sanchit Bajaj <sanchit02@outlook.com>
2020-11-19 13:33:42 +0530

    file structure change

--- js/client.js
+++ js/client.js
@@ -1,41 +1,41 @@
-const socket = io('http://localhost:8000');
+const socket = io("http://localhost:3000");
 
-const form = document.getElementById('send-container');
-const messageInput = document.getElementById('messageInp');
-const messageContainer = document.querySelector('.contain-box');
-const audio = new Audio('swiftly.mp3');
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".contain-box");
+const audio = new Audio("swiftly.mp3");
 
 const name = prompt("Enter your name to join?");
 
 const append = (message, position) => {
-    const messageElement = document.createElement('div');
-    messageElement.innerHTML = message;
-    messageElement.classList.add('message');
-    messageElement.classList.add(position);
-    messageContainer.append(messageElement);
-    if(position == 'left' || position == 'center') {
-        audio.play();
-    }
-}
+  const messageElement = document.createElement("div");
+  messageElement.innerHTML = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+  if (position == "left" || position == "center") {
+    audio.play();
+  }
+};
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const message = messageInput.value;
-    append(`You: ${message}`, 'right');
-    socket.emit('send', message);
-    messageInput.value = '';
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = messageInput.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
+});
 
-socket.emit('new-user-joined', name);
+socket.emit("new-user-joined", name);
 
-socket.on('user-joined', name => {
-    append(`${name} joined the chat`, 'center');
-})
+socket.on("user-joined", (name) => {
+  append(`${name} joined the chat`, "center");
+});
 
-socket.on('receive', data => {
-    append(`${data.name}: ${data.message}`, 'left');
-})
+socket.on("receive", (data) => {
+  append(`${data.name}: ${data.message}`, "left");
+});
 
-socket.on('leave', name => {
-    append(`${name} left the chat`, 'center');
-})
+socket.on("leave", (name) => {
+  append(`${name} left the chat`, "center");
+});
